bc0e2c37508b7a41fc36aa6179f9da36b2403566
hctor12 <hectorgilfuertes12@gmail.com>
2025-12-12 10:20:34 +0100

    viernes

--- NetCoreFundamentos/Form1.Designer.cs
+++ NetCoreFundamentos/Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            txtNombre = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(69, 93);
+            button1.Name = "button1";
+            button1.Size = new Size(143, 53);
+            button1.TabIndex = 0;
+            button1.Text = "Pulsar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(69, 54);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(167, 23);
+            txtNombre.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(69, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(123, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Introduzca su nombre";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(318, 204);
+            Controls.Add(label1);
+            Controls.Add(txtNombre);
+            Controls.Add(button1);
+            Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox txtNombre;
+        private Label label1;
     }
 }
--- NetCoreFundamentos/Form1.cs
+++ NetCoreFundamentos/Form1.cs
@@ -6,5 +6,18 @@ namespace NetCoreFundamentos
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "NetCore Fundamentos - Héctor";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.txtNombre.Location = new Point(10, 30);
+            this.txtNombre.Text = "Soy un texto nuevo!!!";
+            txtNombre.Width = 220;
+            txtNombre.BackColor = Color.Fuchsia;
+        }
     }
 }
